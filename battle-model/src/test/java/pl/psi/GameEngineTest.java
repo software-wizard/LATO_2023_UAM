a37b189c63d484bf3cd2d9371c44264a903945d1
Szymon Szczubkowski <szyszc4@st.amu.edu.pl>
2023-05-16 14:40:00 +0200

    add: more tests

--- battle-model/src/test/java/pl/psi/GameEngineTest.java
+++ battle-model/src/test/java/pl/psi/GameEngineTest.java
@@ -6,20 +6,35 @@ import org.junit.jupiter.api.Test;
 
 import pl.psi.creatures.BattleUnit;
 import pl.psi.creatures.CastleCreatureFactory;
+import pl.psi.creatures.WarMachine;
+import pl.psi.warmachines.WarMachineStatistic;
+import pl.psi.warmachines.WarMachineStats;
 
 /**
  * TODO: Describe this class (The first line - until the first dot - will interpret as the brief description).
  */
 public class GameEngineTest
 {
+    final CastleCreatureFactory creatureFactory = new CastleCreatureFactory();
     @Test
     void shoudWorksHeHe()
     {
-        final CastleCreatureFactory creatureFactory = new CastleCreatureFactory();
         final GameEngine gameEngine =
             new GameEngine( new Hero( List.of( new BattleUnit(creatureFactory.create( 1, false, 5 ) )) ),
                 new Hero( List.of(new BattleUnit(creatureFactory.create( 1, false, 5 ) )) ) );
 
         gameEngine.attack( new Point( 1, 1 ) );
+    }
+
+    @Test
+    void healingShouldNotThrowErrors(){
+        final GameEngine gameEngine =
+                new GameEngine(new Hero(
+                        List.of(
+                                new BattleUnit(new WarMachine.Builder().statistic(WarMachineStatistic.FIRST_AID_TENT).relSkill(0).build()),
+                                new BattleUnit(creatureFactory.create(1, false, 5)))),
+                        new Hero(List.of(new BattleUnit(creatureFactory.create(1, false, 5)))));
+        gameEngine.heal(new Point(1, 1));
+        gameEngine.attack(new Point(1, 1));
     }
 }
--- battle-model/src/test/java/pl/psi/creatures/BattleUnitTest.java
+++ battle-model/src/test/java/pl/psi/creatures/BattleUnitTest.java
@@ -6,6 +6,19 @@ import pl.psi.warmachines.WarMachineStats;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BattleUnitTest {
+
+    @Test
+    void unitShouldOnlyBeOneType(){
+        final Creature aCreature = new Creature.Builder().statistic(CreatureStats.builder().build()).build();
+        final BattleUnit aBattleUnit1 = new BattleUnit(aCreature);
+        assertThat(aBattleUnit1.getCreatureVal()).isNotNull();
+        assertThat(aBattleUnit1.getWarMachineVal()).isNull();
+
+        final WarMachine aWarMachine = new WarMachine.Builder().statistic(WarMachineStats.builder().build()).build();
+        final BattleUnit aBattleUnit2 = new BattleUnit(aWarMachine);
+        assertThat(aBattleUnit2.getCreatureVal()).isNull();
+        assertThat(aBattleUnit2.getWarMachineVal()).isNotNull();
+    }
     @Test
     void unitShouldKnowItsUnderlyingType(){
         final Creature aCreature = new Creature.Builder().statistic(CreatureStats.builder()
