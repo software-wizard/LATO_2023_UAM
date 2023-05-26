3ba9d01226b6f8cf014179552f7f7c47729debc8
Szymon Szczubkowski <szyszc4@st.amu.edu.pl>
2023-05-26 17:58:11 +0200

    add: getCreatures() and getWarMachines() to Hero for economy compatibility

--- battle-model/src/main/java/pl/psi/Hero.java
+++ battle-model/src/main/java/pl/psi/Hero.java
@@ -1,10 +1,13 @@
 package pl.psi;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import pl.psi.creatures.BattleUnit;
 
 import lombok.Getter;
+import pl.psi.creatures.Creature;
+import pl.psi.creatures.WarMachine;
 
 /**
  * TODO: Describe this class (The first line - until the first dot - will interpret as the brief description).
@@ -21,5 +24,13 @@ public class Hero
 
     public boolean isAlly(BattleUnit aBattleUnit1, BattleUnit aBattleUnit2){
         return battleUnits.contains(aBattleUnit1) == battleUnits.contains(aBattleUnit2);
+    }
+
+    public List<Creature> getCreatures(){
+        return battleUnits.stream().filter(BattleUnit::isCreature).map(BattleUnit::getCreatureVal).collect(Collectors.toList());
+    }
+
+    public List <WarMachine> getWarMachines(){
+        return battleUnits.stream().filter(BattleUnit::isWarMachine).map(BattleUnit::getWarMachineVal).collect(Collectors.toList());
     }
 }
