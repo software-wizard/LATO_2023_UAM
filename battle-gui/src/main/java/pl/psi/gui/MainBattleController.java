d1f6a1bdcb78ecfa07011e4b5e8e4c78a88dcb7c
pwieczorek <pwieczorek@psi.pl>
2023-06-07 16:46:51 +0200

    Fix non jafax thread problem

--- battle-gui/src/main/java/pl/psi/gui/MainBattleController.java
+++ battle-gui/src/main/java/pl/psi/gui/MainBattleController.java
@@ -1,5 +1,6 @@
 package pl.psi.gui;
 
+import javafx.application.Platform;
 import pl.psi.*;
 
 import javafx.fxml.FXML;
@@ -12,59 +13,53 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.Optional;
 
-public class MainBattleController implements PropertyChangeListener
-{
+public class MainBattleController implements PropertyChangeListener {
     private final GameEngine gameEngine;
     @FXML
     private GridPane gridMap;
     @FXML
     private Button passButton;
 
-    public MainBattleController( final Hero aPlayer1, final Hero aPlayer2, final ObstaclePlacementList obstaclesList)
-    {
-        gameEngine = new GameEngine( aPlayer1, aPlayer2, obstaclesList);
+    public MainBattleController(final Hero aPlayer1, final Hero aPlayer2, final ObstaclePlacementList obstaclesList) {
+        gameEngine = new GameEngine(aPlayer1, aPlayer2, obstaclesList);
     }
 
     @FXML
-    private void initialize()
-    {
+    private void initialize() {
         refreshGui();
         gameEngine.addObserver(this);
+        passButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> gameEngine.pass());
     }
 
-    private void refreshGui()
-    {
-        gridMap.getChildren()
-                .clear();
-        for( int x = 0; x < 15; x++ )
-        {
-            for( int y = 0; y < 10; y++ )
-            {
-                Point currentPoint = new Point( x, y );
-                Optional<Defendable> object = gameEngine.getObject( currentPoint );
-                final MapTile mapTile = new MapTile( "" );
-                object.ifPresent( c -> mapTile.setName( c.toString() ) );
-                if( gameEngine.isCurrentCreature( currentPoint ) )
-                {
-                    mapTile.setBackground( Color.GREENYELLOW );
+    private void refreshGui() {
+        Platform.runLater(() -> {
+            gridMap.getChildren()
+                    .clear();
+            for (int x = 0; x < 15; x++) {
+                for (int y = 0; y < 10; y++) {
+                    Point currentPoint = new Point(x, y);
+                    Optional<Defendable> object = gameEngine.getObject(currentPoint);
+                    final MapTile mapTile = new MapTile("");
+                    object.ifPresent(c -> mapTile.setName(c.toString()));
+                    if (gameEngine.isCurrentCreature(currentPoint)) {
+                        mapTile.setBackground(Color.GREENYELLOW);
+                    }
+                    if (gameEngine.canMove(currentPoint)) {
+                        mapTile.setBackground(Color.GREY);
+                        mapTile.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+                            gameEngine.move(currentPoint);
+                        });
+                    }
+                    if (gameEngine.canAttack(currentPoint)) {
+                        mapTile.setBackground(Color.RED);
+                        mapTile.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+                            gameEngine.attack(currentPoint);
+                        });
+                    }
+                    gridMap.add(mapTile, x, y);
                 }
-                if( gameEngine.canMove( currentPoint ) )
-                {
-                    mapTile.setBackground( Color.GREY );
-                    mapTile.addEventHandler( MouseEvent.MOUSE_CLICKED, ( e ) -> {
-                        gameEngine.move( currentPoint );
-                    } );
-                }
-                if( gameEngine.canAttack( currentPoint ) )
-                {
-                    mapTile.setBackground( Color.RED );
-                    mapTile.addEventHandler( MouseEvent.MOUSE_CLICKED, ( e ) -> {
-                        gameEngine.attack( currentPoint );
-                    } );
-                }
-                gridMap.add( mapTile, x, y );
             }
-        }
+        });
     }
 
     @Override
